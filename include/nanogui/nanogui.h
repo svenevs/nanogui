7efec9751515ae27bf449ac25a5424e724ecbbaf
sven <sjm324@cornell.edu>
2017-09-17 16:09:46 -0700

    nanogui.h and object.h (just adding file brief)

--- include/nanogui/nanogui.h
+++ include/nanogui/nanogui.h
@@ -1,6 +1,4 @@
 /*
-    nanogui/nanogui.h -- Pull in *everything* from NanoGUI
-
     NanoGUI was developed by Wenzel Jakob <wenzel.jakob@epfl.ch>.
     The widget drawing code is based on the NanoVG demo application
     by Mikko Mononen.
@@ -8,6 +6,11 @@
     All rights reserved. Use of this source code is governed by a
     BSD-style license that can be found in the LICENSE.txt file.
 */
+/**
+ * \file nanogui/nanogui.h
+ *
+ * \brief Pull in **everything** from NanoGUI.
+ */
 
 #pragma once
 
